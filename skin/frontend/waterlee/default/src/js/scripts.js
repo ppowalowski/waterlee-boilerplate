2170ce8f332fd0b7e147b44b3be87435ddff4947
marjanbonus <marjan.bonus@gmail.com>
2015-01-21 22:23:05 +0100

    Qty inc-dec & grid-list icons

--- skin/frontend/waterlee/default/src/js/scripts.js
+++ skin/frontend/waterlee/default/src/js/scripts.js
@@ -1,6 +1,52 @@
-// Select-wrapper for select elements
 
 jQuery(document).ready(function() {
-    jQuery("select").wrap("<div class='select-wrapper'></div>");
-    jQuery("select").after("<i class='fa fa-angle-down'></i>");
+
+  // Select-wrapper for select elements
+
+  jQuery("select").wrap("<div class='select-wrapper'></div>");
+  jQuery("select").after("<i class='fa fa-angle-down'></i>");
+
+  //Flexslider
+
+  jQuery(window).load(function() {
+    jQuery('.product-flexslider').flexslider({
+      animation: "slide",
+      slideshow: false,
+      maxItems: 2,
+      itemWidth: 50
+    });
+  });
+
+  // Product page / wishlist - quantity increase/decrease
+  jQuery(".quantity").append('<i id="add1" class="plus fa fa-plus" />').prepend('<i id="minus1" class="minus fa fa-minus" />');
+  jQuery(".quantity .plus").click(function(){
+    var currentVal = parseInt(jQuery(this).parent().find(".qty").val());
+    if (!currentVal || currentVal=="" || currentVal == "NaN") currentVal = 0;
+    jQuery(this).parent().find(".qty").val(currentVal + 1);
+  });
+
+  jQuery(".quantity .minus").click(function(){
+    var currentVal = parseInt(jQuery(this).parent().find(".qty").val());
+    if (currentVal == "NaN") currentVal = 0;
+    if (currentVal > 1){
+      jQuery(this).parent().find(".qty").val(currentVal - 1);
+    }
+  });
+
+  //Grid / List view
+  jQuery('.view-mode strong.grid').after('<i class="fa fa-th"></i>');
+  jQuery('.view-mode strong.list').after('<i class="fa fa-align-justify"></i>');
+
+  jQuery('.view-mode a.list').each(function() {
+    if (jQuery(this).text() == 'List')
+      jQuery(this).text('');
+      jQuery(this).append('<i class="fa fa-align-justify"></i>');
+    });
+
+  jQuery('.view-mode a.grid').each(function() {
+    if (jQuery(this).text() == 'Grid')
+      jQuery(this).text('');
+      jQuery(this).append('<i class="fa fa-th"></i>');
+  });
+
 });
