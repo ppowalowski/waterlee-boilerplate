2648b37b92364b91fa64922e07f861b58c4e3717
marjanbonus <marjan.bonus@gmail.com>
2015-01-26 21:45:45 +0100

    Top-cart and easyTabs fixes

--- skin/frontend/waterlee/default/src/js/scripts.js
+++ skin/frontend/waterlee/default/src/js/scripts.js
@@ -49,4 +49,13 @@ jQuery(document).ready(function() {
       jQuery(this).append('<i class="fa fa-th"></i>');
   });
 
+  //Top cart
+  jQuery(".top--cart").click(function(e) {
+    e.stopPropagation();
+    jQuery(this).toggleClass('active');
+  });
+  jQuery(document).click(function() {
+    jQuery('.top--cart').removeClass('active');    
+  });
+
 });
